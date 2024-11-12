8f3ac0a9593873c53d94bef27af4e0e4bad71934
AdamRopelewski <adam.ropelewski.kontakt@gmail.com>
2024-11-12 21:16:55 +0100

    Refactor apply_vst function and update plugin parameters func

--- main.py
+++ main.py
@@ -21,23 +21,38 @@ def separate_vocals(input_file, mp3_bitrate=320):
     )
 
 
-def apply_vst(input_file, output_file):
+def prepere_c2_plugin():
     c2_vst_path = "vsts\FabFilter Pro-C 2.vst3"
     c2_plugin = load_plugin(c2_vst_path)
     c2_plugin.active = True
-    c2_plugin.parameters["Ratio"].value = 4.0
-    c2_plugin.parameters["Threshold"].value = -24.0
-    c2_plugin.parameters["Attack"].value = 0.1
-    c2_plugin.parameters["Release"].value = 60.0
-    c2_plugin.parameters["Auto Release"].value = False
-    c2_plugin.parameters["Knee"].value = 2.0
-    c2_plugin.parameters["Lookahead"].value = 10.0
-    c2_plugin.parameters["Hold"].value = 0.0
-    c2_plugin.parameters["Auto Gain"].value = True
+    c2_plugin.parameters["ratio"].value = 4.0
+    c2_plugin.parameters["threshold"].value = -24.0
+    c2_plugin.parameters["attack"].value = 0.1
+    c2_plugin.parameters["release"].value = 60.0
+    c2_plugin.parameters["auto_release"].value = False
+    c2_plugin.parameters["knee"].value = 2.0
+    c2_plugin.parameters["lookahead"].value = 10.0
+    c2_plugin.parameters["hold"].value = 0.0
+    c2_plugin.parameters["auto_gain"].value = True
+    return c2_plugin
+
+
+def apply_vst(input_file, output_path):
+    c2_plugin = prepere_c2_plugin()
 
     board = Pedalboard([c2_plugin])
     audio, sample_rate = sf.read(input_file)
+    processed_audio = board(audio, sample_rate)
+
+    sf.write(
+        output_path,
+        processed_audio,
+        sample_rate,
+        format="MP3",
+        subtype="PCM_16",
+        bitrate="320k",
+    )
 
 
 # separate_vocals("extracted_audio.mp4")
-apply_vst("inputs\extracted_audio.mp4", "outputs\output.wav")
+apply_vst("inputs\extracted_audio.mp3", "outputs\output.mp3")
